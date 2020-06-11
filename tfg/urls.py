3a6bd8f08c43d4a431905dfdd3361ed9ae253133
[Horde] Deivion <736520@unizar.es>
2020-06-11 18:23:46 +0200

    more deployment bugs

--- tfg/urls.py
+++ tfg/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import include,path
 
 from django.views.generic import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
